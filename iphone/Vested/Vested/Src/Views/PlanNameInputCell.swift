78226bec5a0347fad5ca25b25d66bac3017daa34
Noel Curtis <ncurtis@grubhub.com>
2015-01-11 15:42:39 -0500

    Updated with plan input cell.

---
+++ iphone/Vested/Vested/Src/Views/PlanNameInputCell.swift
@@ -0,0 +1,15 @@
+//
+//  InputCell.swift
+//  Vested
+//
+//  Created by ncurtis on 1/11/15.
+//  Copyright (c) 2015 noelcurtis. All rights reserved.
+//
+
+import UIKit
+
+class PlanNameInputCell : UITableViewCell {
+
+    @IBOutlet weak var titleInputField: UITextField!
+
+}
--- iphone/Vested/Vested/Src/Views/RestrictedPlanDetailViewController.swift
+++ iphone/Vested/Vested/Src/Views/RestrictedPlanDetailViewController.swift
@@ -8,14 +8,42 @@
 
 import UIKit
 
-class RestrictedPlanDetailViewController: UIViewController {
+class RestrictedPlanDetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView!.registerNib(UINib(nibName: "PlanNameInputCell", bundle: nil), forCellReuseIdentifier: "plan_name_input_cell")
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    // MARK: - UITableViewDelegate
+    
+    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    // MARK: - UITableViewDatasource
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch(section) {
+            case 0: return 1
+            case 1: return 4
+            case 2: return 2
+            default: return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return self.tableView.dequeueReusableCellWithIdentifier("plan_name_input_cell") as PlanNameInputCell
     }
 
     
